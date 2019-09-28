26d867639db6d98740da050b7f72b05ecd5893aa
t0moko <t0moko@KYROCORE>
2019-09-29 00:49:26 +0200

    initial commit 29/09/2019

---
+++ miniRC_Controller/Controller.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class controller
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.enableMotorButton = New System.Windows.Forms.Button()
+        Me.motorLabel = New System.Windows.Forms.Label()
+        Me.disableMotorButton = New System.Windows.Forms.Button()
+        Me.disableServoButton = New System.Windows.Forms.Button()
+        Me.servoLabel = New System.Windows.Forms.Label()
+        Me.enableServoButton = New System.Windows.Forms.Button()
+        Me.reverseDirectionButton = New System.Windows.Forms.Button()
+        Me.neutralDirectionButton = New System.Windows.Forms.Button()
+        Me.forwardDirectionButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.backGroundWorker = New System.ComponentModel.BackgroundWorker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.comPortButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.comPortTextBox = New System.Windows.Forms.TextBox()
+        Me.disconnectButton = New System.Windows.Forms.Button()
+        Me.steeringBar = New System.Windows.Forms.ProgressBar()
+        Me.speedBar = New System.Windows.Forms.ProgressBar()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'enableMotorButton
+        '
+        Me.enableMotorButton.Location = New System.Drawing.Point(12, 34)
+        Me.enableMotorButton.Name = "enableMotorButton"
+        Me.enableMotorButton.Size = New System.Drawing.Size(83, 36)
+        Me.enableMotorButton.TabIndex = 1
+        Me.enableMotorButton.Text = "Enable Motor"
+        Me.enableMotorButton.UseVisualStyleBackColor = True
+        '
+        'motorLabel
+        '
+        Me.motorLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.motorLabel.Location = New System.Drawing.Point(12, 12)
+        Me.motorLabel.Name = "motorLabel"
+        Me.motorLabel.Size = New System.Drawing.Size(172, 19)
+        Me.motorLabel.TabIndex = 2
+        Me.motorLabel.Text = "Enable or Disable Motor"
+        Me.motorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'disableMotorButton
+        '
+        Me.disableMotorButton.Location = New System.Drawing.Point(101, 34)
+        Me.disableMotorButton.Name = "disableMotorButton"
+        Me.disableMotorButton.Size = New System.Drawing.Size(83, 36)
+        Me.disableMotorButton.TabIndex = 3
+        Me.disableMotorButton.Text = "Disable Motor"
+        Me.disableMotorButton.UseVisualStyleBackColor = True
+        '
+        'disableServoButton
+        '
+        Me.disableServoButton.Location = New System.Drawing.Point(101, 97)
+        Me.disableServoButton.Name = "disableServoButton"
+        Me.disableServoButton.Size = New System.Drawing.Size(83, 36)
+        Me.disableServoButton.TabIndex = 6
+        Me.disableServoButton.Text = "Disable Servo"
+        Me.disableServoButton.UseVisualStyleBackColor = True
+        '
+        'servoLabel
+        '
+        Me.servoLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.servoLabel.Location = New System.Drawing.Point(12, 75)
+        Me.servoLabel.Name = "servoLabel"
+        Me.servoLabel.Size = New System.Drawing.Size(172, 19)
+        Me.servoLabel.TabIndex = 5
+        Me.servoLabel.Text = "Enable or Disable Servo"
+        Me.servoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'enableServoButton
+        '
+        Me.enableServoButton.Location = New System.Drawing.Point(12, 97)
+        Me.enableServoButton.Name = "enableServoButton"
+        Me.enableServoButton.Size = New System.Drawing.Size(83, 36)
+        Me.enableServoButton.TabIndex = 4
+        Me.enableServoButton.Text = "Enable Servo"
+        Me.enableServoButton.UseVisualStyleBackColor = True
+        '
+        'reverseDirectionButton
+        '
+        Me.reverseDirectionButton.Location = New System.Drawing.Point(124, 158)
+        Me.reverseDirectionButton.Name = "reverseDirectionButton"
+        Me.reverseDirectionButton.Size = New System.Drawing.Size(60, 36)
+        Me.reverseDirectionButton.TabIndex = 23
+        Me.reverseDirectionButton.Text = "Reverse"
+        Me.reverseDirectionButton.UseVisualStyleBackColor = True
+        '
+        'neutralDirectionButton
+        '
+        Me.neutralDirectionButton.Location = New System.Drawing.Point(71, 158)
+        Me.neutralDirectionButton.Name = "neutralDirectionButton"
+        Me.neutralDirectionButton.Size = New System.Drawing.Size(50, 36)
+        Me.neutralDirectionButton.TabIndex = 22
+        Me.neutralDirectionButton.Text = "Neutral"
+        Me.neutralDirectionButton.UseVisualStyleBackColor = True
+        '
+        'forwardDirectionButton
+        '
+        Me.forwardDirectionButton.Location = New System.Drawing.Point(12, 158)
+        Me.forwardDirectionButton.Name = "forwardDirectionButton"
+        Me.forwardDirectionButton.Size = New System.Drawing.Size(56, 36)
+        Me.forwardDirectionButton.TabIndex = 21
+        Me.forwardDirectionButton.Text = "Forward"
+        Me.forwardDirectionButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Location = New System.Drawing.Point(12, 136)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(172, 19)
+        Me.Label2.TabIndex = 20
+        Me.Label2.Text = "Direction"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'backGroundWorker
+        '
+        Me.backGroundWorker.WorkerReportsProgress = True
+        Me.backGroundWorker.WorkerSupportsCancellation = True
+        '
+        'Label5
+        '
+        Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label5.Location = New System.Drawing.Point(191, 103)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(134, 19)
+        Me.Label5.TabIndex = 28
+        Me.Label5.Text = "Steering"
+        '
+        'comPortButton
+        '
+        Me.comPortButton.Location = New System.Drawing.Point(190, 34)
+        Me.comPortButton.Name = "comPortButton"
+        Me.comPortButton.Size = New System.Drawing.Size(56, 21)
+        Me.comPortButton.TabIndex = 25
+        Me.comPortButton.Text = "Connect"
+        Me.comPortButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(191, 150)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(134, 19)
+        Me.Label1.TabIndex = 30
+        Me.Label1.Text = "Throttle"
+        '
+        'comPortTextBox
+        '
+        Me.comPortTextBox.Location = New System.Drawing.Point(191, 12)
+        Me.comPortTextBox.Name = "comPortTextBox"
+        Me.comPortTextBox.Size = New System.Drawing.Size(133, 20)
+        Me.comPortTextBox.TabIndex = 32
+        Me.comPortTextBox.Text = "COM19"
+        '
+        'disconnectButton
+        '
+        Me.disconnectButton.Location = New System.Drawing.Point(252, 34)
+        Me.disconnectButton.Name = "disconnectButton"
+        Me.disconnectButton.Size = New System.Drawing.Size(72, 21)
+        Me.disconnectButton.TabIndex = 33
+        Me.disconnectButton.Text = "Disconnect"
+        Me.disconnectButton.UseVisualStyleBackColor = True
+        '
+        'steeringBar
+        '
+        Me.steeringBar.Location = New System.Drawing.Point(191, 125)
+        Me.steeringBar.MarqueeAnimationSpeed = 5
+        Me.steeringBar.Name = "steeringBar"
+        Me.steeringBar.Size = New System.Drawing.Size(133, 22)
+        Me.steeringBar.TabIndex = 34
+        '
+        'speedBar
+        '
+        Me.speedBar.Location = New System.Drawing.Point(190, 172)
+        Me.speedBar.Name = "speedBar"
+        Me.speedBar.Size = New System.Drawing.Size(133, 22)
+        Me.speedBar.TabIndex = 35
+        '
+        'Label3
+        '
+        Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label3.Location = New System.Drawing.Point(190, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(134, 19)
+        Me.Label3.TabIndex = 36
+        Me.Label3.Text = "Firmata Status"
+        '
+        'controller
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(336, 203)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.speedBar)
+        Me.Controls.Add(Me.steeringBar)
+        Me.Controls.Add(Me.disconnectButton)
+        Me.Controls.Add(Me.comPortTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.comPortButton)
+        Me.Controls.Add(Me.reverseDirectionButton)
+        Me.Controls.Add(Me.neutralDirectionButton)
+        Me.Controls.Add(Me.forwardDirectionButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.disableServoButton)
+        Me.Controls.Add(Me.servoLabel)
+        Me.Controls.Add(Me.enableServoButton)
+        Me.Controls.Add(Me.disableMotorButton)
+        Me.Controls.Add(Me.motorLabel)
+        Me.Controls.Add(Me.enableMotorButton)
+        Me.Name = "controller"
+        Me.Text = "miniRC Controller"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents enableMotorButton As Button
+    Friend WithEvents motorLabel As Label
+    Friend WithEvents disableMotorButton As Button
+    Friend WithEvents disableServoButton As Button
+    Friend WithEvents servoLabel As Label
+    Friend WithEvents enableServoButton As Button
+    Friend WithEvents reverseDirectionButton As Button
+    Friend WithEvents neutralDirectionButton As Button
+    Friend WithEvents forwardDirectionButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents backGroundWorker As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label5 As Label
+    Friend WithEvents comPortButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents comPortTextBox As TextBox
+    Friend WithEvents disconnectButton As Button
+    Friend WithEvents steeringBar As ProgressBar
+    Friend WithEvents speedBar As ProgressBar
+    Friend WithEvents Label3 As Label
+End Class
---
+++ miniRC_Controller/Controller.vb
@@ -0,0 +1,342 @@
+﻿Imports SharpDX.XInput 'for using xinput controller
+Imports System.Threading 'running controller checks on seperate thread
+Imports System.ComponentModel
+
+Public Class controller
+
+    Dim Controller As SharpDX.XInput.Controller = New SharpDX.XInput.Controller(UserIndex.One) 'connect to first controller
+    Dim button As Integer
+    Dim gamepadButtons As Array = System.Enum.GetValues(GetType(GamepadButtonFlags)) 'buttons on gamepad
+    Dim gamepadValues() As String
+
+    Dim arduino As Solid.Arduino.SerialConnection 'serial connection to arduino
+    Dim arduinoSession As Solid.Arduino.ArduinoSession 'create new firmata session
+    Dim firmata As Solid.Arduino.Firmata.IFirmataProtocol
+
+    'pins for motor
+    Dim enable_a As Integer = 2
+    Dim enable_b As Integer = 4 '3 originally
+    Dim in_a As Integer = 3 '4 orignally
+    Dim in_b As Integer = 5
+    Dim motorPin As Integer = 11
+
+    'pins for servo
+    Dim servoPin As Integer = 9
+
+    'pins for distance sensor
+    Dim echoPin As Integer = 6
+    Dim trigPin As Integer = 7
+
+    'various constants
+    Dim speedMax As Integer = 255
+    Dim speedMin As Integer = 0
+    Dim steeringMax As Integer = 130
+    Dim steeringMin As Integer = 40
+    Dim steeringCenter As Integer = 80
+
+    'when close com port, disable checking for controller inputs
+    Dim processingInput As Boolean = False
+
+    Function firmataVersion(ByVal msg As Boolean) As String()
+        'gets the firmata version. good check to see if we are connected
+        Dim firmataFw As Solid.Arduino.Firmata.Firmware = firmata.GetFirmware()
+        'if you want a message box with the version or just return values instead
+        If msg = False Then 'false gives values, true give messagebox
+            Return {firmataFw.Name.ToString, firmataFw.MajorVersion.ToString, firmataFw.MinorVersion.ToString}
+        ElseIf msg = True Then
+            MsgBox("Firmata Version: " + firmataFw.Name.ToString + " " + firmataFw.MajorVersion.ToString + "." + firmataFw.MinorVersion.ToString)
+            Return {}
+        Else
+            Return {}
+        End If
+    End Function
+
+    Sub pinMode(ByVal pin As Integer, ByVal mode As String)
+        'basically arduino-like pinMode equivalent
+        If mode = "output" Then
+            arduinoSession.SetDigitalPinMode(pin, Solid.Arduino.Firmata.PinMode.DigitalOutput)
+        ElseIf mode = "input" Then
+            arduinoSession.SetDigitalPinMode(pin, Solid.Arduino.Firmata.PinMode.DigitalInput)
+        ElseIf mode = "pwm" Then
+            arduinoSession.SetDigitalPinMode(pin, Solid.Arduino.Firmata.PinMode.PwmOutput)
+        End If
+    End Sub
+
+    Sub digitalWrite(ByVal pin As Integer, ByVal value As Boolean)
+        'write digital value to port
+        arduinoSession.SetDigitalPin(pin, value)
+    End Sub
+
+    Function digitalAnalogRead(ByVal pin As Integer) As Long
+        'read a pin's value
+        'true or false for bool values, otherwise actual value
+        Return arduinoSession.GetPinState(pin).Value
+    End Function
+
+    Sub analogWrite(ByVal pin As Integer, ByVal value As Long)
+        'write pwm values
+        arduinoSession.SetDigitalPin(pin, value)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'disable buttons till connection
+        disableButtons()
+        'run controller key press detection code
+        backGroundWorker.RunWorkerAsync()
+    End Sub
+
+    'background task uses to report progess
+    Private Sub backGroundWorker_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles backGroundWorker.ProgressChanged
+        'call my processing function
+        processController(CType(e.UserState, List(Of String)))
+    End Sub
+
+    'not needed since the worker never finishes
+    Private Sub backGroundWorker_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles backGroundWorker.RunWorkerCompleted
+    End Sub
+
+    Sub enableButtons()
+        'enable buttons
+        enableMotorButton.Enabled = True
+        disableMotorButton.Enabled = False
+        enableServoButton.Enabled = True
+        disableServoButton.Enabled = False
+        forwardDirectionButton.Enabled = True
+        neutralDirectionButton.Enabled = True
+        reverseDirectionButton.Enabled = True
+    End Sub
+
+    Sub disableButtons()
+        'disable buttons
+        enableMotorButton.Enabled = False
+        disableMotorButton.Enabled = False
+        enableServoButton.Enabled = False
+        disableServoButton.Enabled = False
+        forwardDirectionButton.Enabled = False
+        neutralDirectionButton.Enabled = False
+        reverseDirectionButton.Enabled = False
+    End Sub
+
+    Sub enableMotor()
+        digitalWrite(enable_a, True)
+        digitalWrite(enable_b, True)
+        pinMode(motorPin, "pwm")
+        enableButtons("en")
+    End Sub
+
+    Sub disableMotor()
+        digitalWrite(enable_a, False)
+        digitalWrite(enable_b, False)
+        pinMode(motorPin, "input")
+        enableButtons("dis")
+    End Sub
+
+    Sub setDirection(ByVal dir As String)
+        If dir = "forward" Then
+            digitalWrite(in_a, True)
+            digitalWrite(in_b, False)
+            directionButtons("f")
+        ElseIf dir = "reverse" Then
+            digitalWrite(in_a, False)
+            digitalWrite(in_b, True)
+            directionButtons("r")
+        ElseIf dir = "neutral" Then
+            digitalWrite(in_a, False)
+            digitalWrite(in_b, False)
+            directionButtons("n")
+
+        End If
+    End Sub
+
+    Sub processController(ByVal items As List(Of String))
+        '[Button 0, Value 1, LT 2, Value 3, RT 4, Value 5,  LX 6, Value 7, LY 8, Value 9, RX 10, Value 11, RY 12, Value 13]
+
+        'extract values from array for further use
+        Dim button As String = items(1).TrimStart(CType(" ", Char()))
+        Dim lTrig As Integer = CInt(items(3))
+        Dim rTrig As Integer = CInt(items(5))
+        Dim lX As Integer = CInt(items(7))
+        Dim lY As Integer = CInt(items(9))
+        Dim rX As Integer = CInt(items(11))
+        Dim rY As Integer = CInt(items(13))
+        Dim steering As Integer = steeringCenter
+
+        'steering has a center, the analog sticks center are not zero so to copensate
+        If lX > -32767 And lX <= -127 Then
+            steering = valueMap(-32767, -127, steeringMin, steeringCenter, lX)
+        ElseIf lX > -127 And lX <= 130 Then
+            steering = steeringCenter
+        Else
+            steering = valueMap(130, 32767, steeringCenter, steeringMax, lX)
+        End If
+
+        'map values of trigger to min and max
+        Dim speed As Integer = valueMap(0, 255, speedMin, speedMax, rTrig)
+
+        If button = "Start" Then
+            'if press start the do the followjng
+
+            Dim comport As String = comPortTextBox.Text
+            enableButtons()
+            arduino = New Solid.Arduino.SerialConnection(comport, Solid.Arduino.SerialBaudRate.Bps_57600)
+            arduinoSession = New Solid.Arduino.ArduinoSession(arduino, 250)
+            firmata = CType(arduinoSession, Solid.Arduino.Firmata.IFirmataProtocol)
+
+            'check if we are connected
+            Label3.Text = firmataVersion(False)(0)
+            enableButtons("start")
+
+            'for some reason the motor gets enabled at startup so just to make sure its in neutral pos
+            digitalWrite(in_a, False)
+            digitalWrite(in_b, False)
+            processingInput = True
+
+        ElseIf button = "Back" Then
+
+            'for disconnection from arduino
+            arduinoSession.ResetBoard()
+            arduino.Close()
+            enableButtons("back")
+            processingInput = False
+            disableButtons()
+
+        End If
+
+        'check if we need to process gamepad inputs
+        If processingInput = True Then
+            If button = "Y" Then
+                'forward direction
+                setDirection("forward")
+            ElseIf button = "A" Then
+                'reverse direction
+                setDirection("reverse")
+            ElseIf button = "B" Then
+                'neutral
+                setDirection("neutral")
+            ElseIf button = "X" Then
+                'lower max speed
+                speedMax = 64
+                Label1.Text = speedMax.ToString
+            ElseIf button = "LeftShoulder" Then
+                'set speed to max
+                speedMax = 255
+                Label1.Text = speedMax.ToString
+            ElseIf button = "RightShoulder" Then
+                'set speed to half of max
+                speedMax = 128
+                Label1.Text = speedMax.ToString
+            ElseIf button = "DPadLeft" Then
+                'enable motor
+                enableMotor()
+            ElseIf button = "DPadRight" Then
+                'disable motor
+                disableMotor()
+            ElseIf button = "DPadUp" Then
+                'enable servo
+                arduinoSession.ConfigureServo(servoPin, 540, 2400)
+                enableButtons("sen")
+            ElseIf button = "DPadDown" Then
+                'disable servo
+                pinMode(servoPin, "input")
+                enableButtons("sdis")
+            End If
+
+            'write pwm values to respective component
+            analogWrite(servoPin, steering)
+            analogWrite(motorPin, speed)
+
+            'update progress bars
+            steeringBar.Value = Math.Abs(valueMap(40, 130, 0, 100, steering))
+            speedBar.Value = CInt((speed / speedMax) * 100)
+        End If
+
+    End Sub
+
+    Sub directionButtons(ByVal dir As String)
+        'if forward is chosen, disable its button and enable rest
+        If dir = "f" Then
+            forwardDirectionButton.Enabled = False
+            neutralDirectionButton.Enabled = True
+            reverseDirectionButton.Enabled = True
+        ElseIf dir = "r" Then
+            forwardDirectionButton.Enabled = True
+            neutralDirectionButton.Enabled = True
+            reverseDirectionButton.Enabled = False
+        ElseIf dir = "n" Then
+            forwardDirectionButton.Enabled = True
+            neutralDirectionButton.Enabled = False
+            reverseDirectionButton.Enabled = True
+
+        End If
+    End Sub
+
+    Sub enableButtons(ByVal dir As String)
+        'depending on what to enable, this enables and disable respective buttons
+        If dir = "en" Then
+            enableMotorButton.Enabled = False
+            disableMotorButton.Enabled = True
+        ElseIf dir = "dis" Then
+            enableMotorButton.Enabled = True
+            disableMotorButton.Enabled = False
+        ElseIf dir = "sen" Then
+            enableServoButton.Enabled = False
+            disableServoButton.Enabled = True
+        ElseIf dir = "sdis" Then
+            enableServoButton.Enabled = True
+            disableServoButton.Enabled = False
+        ElseIf dir = "start" Then
+            comPortButton.Enabled = False
+            disconnectButton.Enabled = True
+        ElseIf dir = "back" Then
+            comPortButton.Enabled = True
+            disconnectButton.Enabled = False
+        End If
+
+    End Sub
+
+    Function valueMap(ByVal in_min As Integer, ByVal in_max As Integer, ByVal out_min As Integer, ByVal out_max As Integer, ByVal value As Integer) As Integer
+        'maps a range from one to another
+        Dim y As Integer
+        y = CInt(((value - in_min) / (in_max - in_min)) * (out_max - out_min) + out_min)
+        Return y
+    End Function
+
+    Private Sub backGroundWorker_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles backGroundWorker.DoWork
+        'check controller for keys
+        Dim prevState As SharpDX.XInput.State = Controller.GetState()
+        Dim currState As SharpDX.XInput.State
+        Dim valueArray() As String
+        Dim processedArray As New List(Of String)
+
+        'while controller is connected
+        While Controller.IsConnected
+            currState = Controller.GetState()
+            'check if state is different
+            If prevState.PacketNumber <> currState.PacketNumber Then
+                valueArray = currState.Gamepad.ToString.Split(CType(",", Char()))
+
+                For Each item As String In valueArray
+                    'add key and values
+                    For Each subitem As String In item.Split(CType(":", Char()))
+                        processedArray.Add(subitem)
+                    Next
+                Next
+
+                'calls this function with array of valyes
+                backGroundWorker.ReportProgress(0, processedArray)
+
+                'vb,net will keep increasing the array each time, so zero it to start again
+                processedArray = New List(Of String)
+
+            End If
+            'thread to sleep
+            Thread.Sleep(10)
+            prevState = currState
+        End While
+
+    End Sub
+
+    Private Sub ComPortButton_Click(sender As Object, e As EventArgs) Handles comPortButton.Click
+
+    End Sub
+End Class
